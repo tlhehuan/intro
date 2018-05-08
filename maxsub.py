602db7cd23aed09a2186f61eb3f275f8ac85b64c
hehuan <hehuan@hehuandeMacBook-Pro.local>
2018-05-09 00:48:19 +0800

    maximum subarray

--- maxsub.py
+++ maxsub.py
@@ -35,25 +35,45 @@ def find_max_subarray(a, low, high):
         else:
                 return cross_low, cross_high, cross_sum
 
-def find_max_subarray_linear_time(a, low, high):
-        max_sum = N_INF
-        p = a[low]
+
+def find_max_subarray_linear_time_simple(a):
+        """
+        来自wikipedia
+        Because of the way this algorithm uses optimal substructures (the maximum subarray
+        ending at each position is calculated in a simple way from a related but smaller
+        and overlapping subproblem: the maximum subarray ending at the previous position) 
+        this algorithm can be viewed as a simple/trivial example of dynamic programming.
+        """
         
-        for j in xrange(low+1, high+1):
-                p = max(a[j], p + a[j])
-                if p > max_sum:
-                        max_sum = p
-        return max_sum
+        b = [None]*len(a)       #the maximum subarray sum ending at position i for all positions
+        b[0] = a[0]
+        for i in xrange(1, len(a)):
+                b[i] = max(b[i-1]+a[i], a[i])
+        return max(b)
+
+def find_max_subarray_linear_time(a):
+        b = [None]*len(a)
+        b[0] = [0, 0, a[0]]     #left, right, sum
+        for i in xrange(1, len(a)):
+                if b[i-1][2] < 0:
+                        b[i] = [i, i, a[i]]
+                else:
+                        b[i] = b[i-1][:]
+                        b[i][1] += 1
+                        b[i][2] += a[i]
+        return max(b, key = lambda x : x[2])
 
 if __name__ == "__main__":
         lst = [1,2,3,-10,-4,100,200,1,2,3,-1,-2,-40,300,-1,-2,-20]
         low, high, sum = find_max_subarray(lst, 0, len(lst)-1)
-        print "maximum subarray:\t", lst[low:high+1]
-        print "recursive result:\t", sum
-        print "linear time result:\t", find_max_subarray_linear_time(lst, 0, len(lst)-1)
+        print "recursive result:\t", sum, lst[low:high+1]
+
+        low, high, sum = find_max_subarray_linear_time(lst)
+        print "linear time result:\t", sum, lst[low:high+1]
 
         lst = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]      #样例（股票价格变化的最大子数组问题）
         low, high, sum = find_max_subarray(lst, 0, len(lst)-1)
-        print "maximum subarray:\t", lst[low:high+1]
-        print "recursive result:\t", sum
-        print "linear time result:\t", find_max_subarray_linear_time(lst, 0, len(lst)-1)
+        print "recursive result:\t", sum, lst[low:high+1]
+
+        low, high, sum = find_max_subarray_linear_time(lst)
+        print "linear time result:\t", sum, lst[low:high+1]
